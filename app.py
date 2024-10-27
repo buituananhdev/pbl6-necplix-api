a86c9d907296350b27157d778e61f83fbced72ed
TuanAnh1003 <anhaanh2003@gmail.com>
2024-10-27 12:49:10 +0700

    fix: add cors

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.jwt_bearer import JWTBearer
 from config.config import initiate_database
 from routes.user import router as UserRouter
 from routes.student import router as StudentRouter
+from routes.movie import router as MovieRouter
 
 app = FastAPI()
 
 token_listener = JWTBearer()
 
+origins = [
+    "http://localhost:3000",
+    "https://necplix.site",
+    "http://necplix.site"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def start_database():
@@ -20,5 +35,6 @@ async def read_root():
     return {"message": "Welcome to this fantastic app."}
 
 
-app.include_router(UserRouter, tags=["User"], prefix="/users")
+app.include_router(UserRouter, tags=["Users"], prefix="/users")
+app.include_router(MovieRouter, tags=["Movies"], prefix="/movies")
 app.include_router(StudentRouter,tags=["Students"],prefix="/students",dependencies=[Depends(token_listener)],)
