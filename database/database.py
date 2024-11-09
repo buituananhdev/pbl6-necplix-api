2748822bf014774676a7b7695596362fb0898398
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-11-09 17:47:18 +0700

    fix: missing query

--- database/database.py
+++ database/database.py
@@ -4,15 +4,44 @@ from beanie import PydanticObjectId
 
 from models.user import User
 from models.student import Student
+from models.movies import Movie
 
 user_collection = User
 student_collection = Student
+movie_collection = Movie 
 
 
 async def add_user(new_user: User) -> User:
     user = await new_user.create()
     return user
 
+async def retrieve_movies() -> List[Movie]:
+    movies = await movie_collection.all().to_list()
+    return movies
+
+async def retrieve_movie(id: PydanticObjectId) -> Movie:
+    movie = await movie_collection.get(id)
+    if movie:
+        return movie
+
+async def add_movie(new_movie: Movie) -> Movie:
+    movie = await new_movie.create()
+    return movie
+
+async def delete_movie(id: PydanticObjectId) -> bool:
+    movie = await movie_collection.get(id)
+    if movie:
+        await movie.delete()
+        return True
+    
+async def update_movie_data(id: PydanticObjectId, data: dict) -> Union[bool, Movie]:
+    des_body = {k: v for k, v in data.items() if v is not None}
+    update_query = {"$set": {field: value for field, value in des_body.items()}}
+    movie = await movie_collection.get(id)
+    if movie:
+        await movie.update(update_query)
+        return movie
+    return False
 
 async def retrieve_students() -> List[Student]:
     students = await student_collection.all().to_list()
--- schemas/movie.py
+++ schemas/movie.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Any
 
 
 class MovieData(BaseModel):
@@ -45,7 +45,6 @@ class MovieData(BaseModel):
 class Genre(BaseModel):
     id: int
     name: str
-
 
 class ProductionCompany(BaseModel):
     id: int
@@ -237,5 +236,58 @@ class MovieDetail(BaseModel):
                         }
                     ]
                 }
+            }
+        }
+
+class Response(BaseModel):
+    status_code: int
+    response_type: str
+    description: str
+    data: Optional[Any]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "status_code": 200,
+                "response_type": "success",
+                "description": "Operation successful",
+                "data": "Sample data",
+            }
+        }
+
+class UpdateMovieModel(BaseModel):
+    id: Optional[int] = None
+    title: Optional[str] = None
+    original_title: Optional[str] = None
+    overview: Optional[str] = None
+    poster_path: Optional[str] = None
+    backdrop_path: Optional[str] = None
+    adult: Optional[bool] = True
+    original_language: Optional[str] = None
+    genre_ids: Optional[list[int]] = []
+    popularity: Optional[float] = None
+    release_date: Optional[str] = None
+    vote_average: Optional[float] = None
+    vote_count: Optional[int] = None
+
+    class Collection:
+        name = "movie"
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 912649,
+                "title": "Venom: The Last Dance",
+                "original_title": "Venom: The Last Dance",
+                "overview": "Eddie and Venom are on the run. Hunted by both of their worlds and with the net closing in, the duo are forced into a devastating decision that will bring the curtains down on Venom and Eddie's last dance.",
+                "poster_path": "/k42Owka8v91trK1qMYwCQCNwJKr.jpg",
+                "backdrop_path": "/3V4kLQg0kSqPLctI5ziYWabAZYF.jpg",
+                "adult": False,
+                "original_language": "en",
+                "genre_ids": [28, 878, 12],
+                "popularity": 5868.656,
+                "release_date": "2024-10-22",
+                "vote_average": 6.3,
+                "vote_count": 163
             }
         }
